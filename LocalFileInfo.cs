7571827b7a0ee39801b7c0aec1899c4a1f05609a
Rampastring <rampastring@outlook.com>
2017-10-27 00:57:56 +0300

    Added LocalFileInfo and RemoteFileInfo

---
+++ LocalFileInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTAUpdater2
+{
+    /// <summary>
+    /// Represents a file on the local system.
+    /// </summary>
+    class LocalFileInfo
+    {
+        public string FilePath { get; private set; }
+        public byte[] Hash { get; private set; }
+    }
+}
---
+++ RemoteFileInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTAUpdater2
+{
+    /// <summary>
+    /// Represents a file on the update server.
+    /// </summary>
+    class RemoteFileInfo
+    {
+        public string FilePath { get; private set; }
+        public byte[] Hash { get; private set; }
+        public bool Compressed { get; private set; }
+    }
+}

927e0b653259122e1aedf10b2dca574b312b832d
Rampastring <rampastring@outlook.com>
2023-08-28 00:00:06 +0300

    Add build script for SecondStageUpdater

---
+++ SecondStageUpdater/build.bat
@@ -0,0 +1,1 @@
+dotnet publish SecondStageUpdater.csproj -c Release -p:PublishSingleFile=true -p:PublishTrimmed=false -p:IncludeNativeLibrariesForSelfExtract=true -p:CopyOutputSymbolsToPublishDirectory=false -r win-x64 --self-contained=false

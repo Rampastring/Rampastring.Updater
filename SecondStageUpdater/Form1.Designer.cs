6b7115a72e4454b9544283d2ec551a59445b285f
Rampastring <rampastring@outlook.com>
2025-02-03 16:29:25 +0200

    Disable auto-scaling in second stage updater window

--- SecondStageUpdater/Form1.Designer.cs
+++ SecondStageUpdater/Form1.Designer.cs
@@ -28,61 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.lblDescription = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            listBox1 = new System.Windows.Forms.ListBox();
+            lblDescription = new System.Windows.Forms.Label();
+            SuspendLayout();
             // 
             // listBox1
             // 
-            this.listBox1.BackColor = System.Drawing.Color.Black;
-            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
-            this.listBox1.ForeColor = System.Drawing.Color.LimeGreen;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(0, 112);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(584, 249);
-            this.listBox1.TabIndex = 0;
-            this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
-            this.listBox1.MeasureItem += new System.Windows.Forms.MeasureItemEventHandler(this.listBox1_MeasureItem);
-            this.listBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.GenericKeyPress);
+            listBox1.BackColor = System.Drawing.Color.Black;
+            listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            listBox1.ForeColor = System.Drawing.Color.LimeGreen;
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new System.Drawing.Point(0, 74);
+            listBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new System.Drawing.Size(584, 287);
+            listBox1.TabIndex = 0;
+            listBox1.DrawItem += listBox1_DrawItem;
+            listBox1.MeasureItem += listBox1_MeasureItem;
+            listBox1.KeyPress += GenericKeyPress;
             // 
             // lblDescription
             // 
-            this.lblDescription.AutoSize = true;
-            this.lblDescription.BackColor = System.Drawing.Color.Transparent;
-            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescription.Location = new System.Drawing.Point(176, 70);
-            this.lblDescription.Name = "lblDescription";
-            this.lblDescription.Size = new System.Drawing.Size(221, 16);
-            this.lblDescription.TabIndex = 1;
-            this.lblDescription.Text = "Finishing update, please wait...";
+            lblDescription.AutoSize = true;
+            lblDescription.BackColor = System.Drawing.Color.Transparent;
+            lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0);
+            lblDescription.Location = new System.Drawing.Point(174, 31);
+            lblDescription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblDescription.Name = "lblDescription";
+            lblDescription.Size = new System.Drawing.Size(220, 16);
+            lblDescription.TabIndex = 1;
+            lblDescription.Text = "Finishing update, please wait...";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(584, 361);
-            this.Controls.Add(this.lblDescription);
-            this.Controls.Add(this.listBox1);
-            this.ForeColor = System.Drawing.Color.LimeGreen;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Updater";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.Shown += new System.EventHandler(this.Form1_Shown);
-            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.GenericKeyPress);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            BackColor = System.Drawing.Color.Black;
+            ClientSize = new System.Drawing.Size(584, 361);
+            Controls.Add(lblDescription);
+            Controls.Add(listBox1);
+            ForeColor = System.Drawing.Color.LimeGreen;
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Updater";
+            FormClosing += Form1_FormClosing;
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
+            Shown += Form1_Shown;
+            KeyPress += GenericKeyPress;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
